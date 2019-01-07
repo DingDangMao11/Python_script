21d2472d722cf421fb60a007a98427a946b28cab
DingDangMao11 <40315197+DingDangMao11@users.noreply.github.com>
2019-01-07 10:38:41 +0800

    Update 返回脚本名称.py

--- 返回脚本名称.py
+++ 返回脚本名称.py
@@ -1,18 +1,15 @@
 from flask import Flask, current_app
 from werkzeug.local import Local
-
 # 只要绑定在Local对象上的属性
 # 在每个线程中都是隔离的
 
 app = Flask(__name__)
-
 # 栈
 #方法1
 # app_context = app.app_context()
 # app_context.push()
 # print(current_app.name)
 #方法2
-
 with app.app_context():
     print(current_app.name)
 
@@ -20,7 +17,6 @@ with app.app_context():
 def index():
     print(current_app.name)
     return 'Hello World!'
-
 
 if __name__ == '__main__':
     app.run()
